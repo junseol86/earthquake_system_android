d118ab949f907a37eede51a29b1a926701b5a447
Ko Hyunmin <junseol86@gmail.com>
2019-01-14 23:26:18 +0900

    UPDATE naver map api key

--- app/src/main/java/com/akadev/hyeonmin/eq_sys_android/util/Const.kt
+++ app/src/main/java/com/akadev/hyeonmin/eq_sys_android/util/Const.kt
@@ -2,7 +2,7 @@ package com.akadev.hyeonmin.eq_sys_android.util
 
 object Const {
     val apiUrl = "http://35.229.252.63:8080/"
-    var clientId = "M091CYFEwJIfJ0NTem9b"
+    var clientId = "g6iy074w1z"
     var initLat = 35.74095
     var initLng = 129.4432829
 }
